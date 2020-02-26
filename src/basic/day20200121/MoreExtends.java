40c766882343e21519c38257a5fd3078200f3657
xurunchen <xurunchen@haylion.cn>
2020-02-26 13:20:46 +0800

    提交代码

---
+++ src/basic/day20200121/MoreExtends.java
@@ -0,0 +1,20 @@
+package basic.day20200121;
+
+public class MoreExtends {
+    public static void main(String[] args) {
+        System.out.println("I don't write JAVA for a few month");
+        System.out.println("is this keyboard with problem double click?");
+        System.out.println("it seems it is extrodonary");
+        System.out.println();
+    }
+}
+
+class teacher{
+    int face;
+
+}
+class sexy_woman extends teacher{
+
+}
+
+
---
+++ src/basic/day20200210/Child.java
@@ -0,0 +1,11 @@
+package basic.day20200210;
+
+public class Child extends Parent{
+    int num = 22;
+    int numParent = 23;
+
+//    @Override
+//    public int getNum() {
+//        return num;
+//    }
+}

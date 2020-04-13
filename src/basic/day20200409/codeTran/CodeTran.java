414d9d13a7bd27a840148bbd9adedb43b3826a57
xurunchen <xurunchen@haylion.cn>
2020-04-13 13:19:51 +0800

    提交代码

--- src/basic/day20200409/codeTran/CodeTran.java
+++ src/basic/day20200409/codeTran/CodeTran.java
@@ -1,15 +1,32 @@
 package basic.day20200409.codeTran;
 
-import jdk.internal.util.xml.impl.Input;
-
 import java.io.*;
 
 public class CodeTran {
-    public static boolean translate(String source, String srcCode, String target, String tarCode) {
-        try(BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(source), srcCode));
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(target), tarCode));){
+    public static final int BUFFER_SIZE = 1024;
+
+
+    public static boolean translateByBuffer(String source, String srcCode, String target, String tarCode) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(source), srcCode));
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(target), tarCode))) {
             int len = 0;
-            char[] chars = new char[1024];
+            char[] chars = new char[BUFFER_SIZE];
+            while ((len = in.read(chars)) != -1) {
+                out.write(chars, 0, len);
+            }
+            out.flush();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean translateNoBuffer(String source, String srcCode, String target, String tarCode){
+        try(InputStreamReader in = new InputStreamReader(new FileInputStream(source), srcCode);
+            OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(target), tarCode)){
+            int len = 0;
+            char[] chars = new char[BUFFER_SIZE];
             while((len = in.read(chars)) != -1) {
                 out.write(chars, 0, len);
             }
@@ -19,7 +36,5 @@ public class CodeTran {
             e.printStackTrace();
             return false;
         }
-
-
     }
 }
--- test/basic/day20200409/codeTran/CodeTranTest.java
+++ test/basic/day20200409/codeTran/CodeTranTest.java
@@ -7,10 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class CodeTranTest {
 
     @Test
-    void translate() {
-        assertTrue(CodeTran.translate("test/basic/day20200409/codeTran/utf_8.txt", "utf-8",
-                "test/basic/day20200409/codeTran/utf_8.txt", "gbk"));
-        assertTrue(CodeTran.translate("test/basic/day20200409/codeTran/gbk.txt", "gbk",
+    void translate1() {
+        long begin = System.currentTimeMillis();
+        assertTrue(CodeTran.translateByBuffer("test/basic/day20200409/codeTran/utf_8.txt", "utf-8",
+                "test/basic/day20200409/codeTran/gbk.txt", "gbk"));
+        assertTrue(CodeTran.translateByBuffer("test/basic/day20200409/codeTran/gbk.txt", "gbk",
                 "test/basic/day20200409/codeTran/utf_8_target.txt", "utf-8"));
+        long end = System.currentTimeMillis();
+        System.out.println("使用缓冲区耗费毫秒:" + (end - begin));
+    }
+
+    @Test
+    void translate2() {
+        long begin = System.currentTimeMillis();
+        assertTrue(CodeTran.translateNoBuffer("test/basic/day20200409/codeTran/utf_8.txt", "utf-8",
+                "test/basic/day20200409/codeTran/gbk.txt", "gbk"));
+        assertTrue(CodeTran.translateByBuffer("test/basic/day20200409/codeTran/gbk.txt", "gbk",
+                "test/basic/day20200409/codeTran/utf_8_target.txt", "utf-8"));
+        long end = System.currentTimeMillis();
+        System.out.println("不使用缓冲区耗费毫秒:" + (end - begin));
     }
 }
